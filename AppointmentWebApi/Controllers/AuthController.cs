cb283f7b167ee3ef725b1bc27d03356c1f61aa0d
Michael Samir <michael.samir.jacob@gmail.com>
2024-01-26 19:06:49 +0200

    Implement Auth

---
+++ AppointmentWebApi/Controllers/AuthController.cs
@@ -0,0 +1,162 @@
+﻿using AppointmentWebApi.Core.Dtos;
+using AppointmentWebApi.Core.OtherObjects;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AppointmentWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+        }
+
+        // Seed roles in DB
+        [HttpPost]
+        [Route("seed-roles")]
+        public async Task<IActionResult> SeedRoles()
+        {
+            string result = string.Empty;
+            bool isAdminExists = await _roleManager.RoleExistsAsync(StaticRoles.ADMIN);
+            bool isServantExists = await _roleManager.RoleExistsAsync(StaticRoles.SERVANT);
+
+            if (isAdminExists)
+                result += "Admin already exists in DB.";
+            else
+            {
+                var createResult = await _roleManager.CreateAsync(new IdentityRole(StaticRoles.ADMIN));
+                if (createResult.Succeeded)
+                    result = string.IsNullOrEmpty(result) ? "ADMIN role created successfully." : result + "\nADMIN role created successfully.";
+                else if (createResult.Errors != null && createResult.Errors.Count() > 0)
+                {
+                    Array.ForEach(createResult.Errors.ToArray(), d => result += "\n" + d.Description);
+                }
+            }
+
+            if (isServantExists)
+                result = string.IsNullOrEmpty(result) ? "Servant already exists in DB." : result + "\nServant already exists in DB.";
+            else
+            {
+                var createResult = await _roleManager.CreateAsync(new IdentityRole(StaticRoles.SERVANT));
+                if (createResult.Succeeded)
+                    result = string.IsNullOrEmpty(result) ? "SERVANT role created successfully." : result + "\nSERVANT role created successfully.";
+                else if (createResult.Errors != null && createResult.Errors.Count() > 0)
+                {
+                    Array.ForEach(createResult.Errors.ToArray(), d => result += "\n" + d.Description);
+                }
+            }
+
+            return Ok(result);
+        }
+
+        // Register
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
+        {
+            var userInDB = await _userManager.FindByNameAsync(registerDto.UserName);
+
+            if (userInDB != null)
+                return BadRequest("UserName already exists in DB");
+
+            IdentityUser newUser = new IdentityUser()
+            {
+                UserName = registerDto.UserName,
+                Email = registerDto.Email,
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+
+            var createUserResult = await _userManager.CreateAsync(newUser, registerDto.Password);
+
+            if (!createUserResult.Succeeded)
+            {
+                string errorString = "User creation failed!";
+                if (createUserResult.Errors != null && createUserResult.Errors.Count() > 0)
+                {
+                    errorString += " with Error(s):";
+                    foreach (var error in createUserResult.Errors)
+                    {
+                        errorString += "\n" + error.Description;
+                    }
+                }
+                return BadRequest(errorString);
+            }
+
+            // Add default SERVANT role to all users
+            await _userManager.AddToRoleAsync(newUser, StaticRoles.SERVANT);
+
+            return Ok("User created successfully.");
+
+        }
+
+        // Login
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            var userInDB = await _userManager.FindByNameAsync(loginDto.UserName);
+
+            if (userInDB == null)
+                return Unauthorized("Invalid credentials!");
+
+            var isPasswordCorrect = await _userManager.CheckPasswordAsync(userInDB, loginDto.Password);
+            if (!isPasswordCorrect)
+                return Unauthorized("Invalid credentials!");
+
+            var userRoles = await _userManager.GetRolesAsync(userInDB);
+
+            var authClaims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name,userInDB.UserName),
+                new Claim(ClaimTypes.NameIdentifier,userInDB.Id),
+                new Claim("JWTID",Guid.NewGuid().ToString())
+            };
+
+            foreach (var userRole in userRoles)
+            {
+                authClaims.Add(new Claim(ClaimTypes.Role, userRole));
+            }
+
+            var token = GenerateNewJsonWebToken(authClaims);
+            return Ok(token);
+        }
+
+        private string GenerateNewJsonWebToken(List<Claim> claims)
+        {
+            var authSecret = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+
+            var tokenObject = new JwtSecurityToken(
+                issuer: _configuration["JWT:ValidIssuer"],
+                audience: _configuration["JWT:ValidAudience"],
+                expires: DateTime.Now.AddHours(1),
+                claims: claims,
+                signingCredentials: new SigningCredentials(authSecret, SecurityAlgorithms.HmacSha256)
+                );
+
+            string token = new JwtSecurityTokenHandler().WriteToken(tokenObject);
+
+            return token;
+        }
+
+        //ValidateIssuer = true,
+        //    ValidateAudience = true,
+        //    ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+        //    ValidAudience = builder.Configuration["JWT:ValidAudience"],
+        //    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
+    }
+}
--- AppointmentWebApi/Core/Dtos/LoginDto.cs
+++ AppointmentWebApi/Core/Dtos/LoginDto.cs
@@ -4,11 +4,11 @@ namespace AppointmentWebApi.Core.Dtos
 {
     public class LoginDto
     {
-        //[Required(ErrorMessage = "UserName is required")]
-        //public string UserName { get; set; }
+        [Required(ErrorMessage = "UserName is required")]
+        public string UserName { get; set; }
 
-        [Required(ErrorMessage = "Mobile is required")]
-        public string Mobile { get; set; }
+        //[Required(ErrorMessage = "Mobile is required")]
+        //public string Mobile { get; set; }
 
         [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
